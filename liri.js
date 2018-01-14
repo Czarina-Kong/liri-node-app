e137162ea806e76de8ed1efedf72a9a9df4ceb21
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-01-14 10:11:13 -0800

    fix typo in keys.js and create myTwitter function

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-console.log('liri.js--running')
+// console.log('liri.js--running')
 require("dotenv").config()
 
 // Let's make some variables
@@ -6,50 +6,64 @@ require("dotenv").config()
 // export modules
 var fs = require('fs')
 var request = require('request')
-var Twitter = require('twitter')
-var SpotifyWebApi = require('spotify-web-api-node')
+var twitter = require('twitter')
+// var spotify = require('spotify')
 var keys = require("./keys.js")
 // variables to store commands and user inputs
 var command = process.argv[2]
 var userInput = process.argv[3]
-// array to store tweets
-var tweets = []
 // default searches when process.argv[3] is empty
 var defaultSong = "The Sign"
 var defaultMovie = "Mr. Nobody"
 // variables to access keys
-var client = new Twitter(keys.twitter)
-var spotifyApi = new SpotifyWebApi(keys.spotify)
+var client = new twitter(keys.twitter)
+// var spotify = new spotify(keys.spotify)
 
 // Let's make some functions
 
 //function to run commands
-function runCommands(command, userInput){
-	console.log('runCommands function is working')
-	console.log(userInput)
-	switch(command){
-		case 'my-tweets':
-			getTweets()
-			break
-		case 'spotify-this-song':
-			//If user has not specified a song , use default
-			if(userInput === undefined){
-				userInput = defaultSong
-			}     
-			searchSong(userInput)
-			break
-		case 'movie-this':
-			//If user has not specified a movie Name , use default
-			if(userInput === undefined){
-				userInput = defaultMovie
-			}    
-			searchMovie(userInput)
-			break
-		case 'do-what-it-says':
-			doWhatItSays()
-			break
-		default: 
-			console.log("Please type any of the following commands: my-tweets spotify-this-song movie-this do-what-it-says.")
-	}
+
+	// console.log('runCommands function is working')
+switch(command){
+	case 'my-tweets':
+		myTweets();
+		break;
+	case 'spotify-this-song':
+		//If user has not specified a song , use default
+		if(userInput === undefined){
+			userInput = defaultSong;
+		}
+		console.log('userInput: '+userInput)     
+		searchSong(userInput);
+		break;
+	case 'movie-this':
+		//If user has not specified a movie Name , use default
+		if(userInput === undefined){
+			userInput = defaultMovie;
+		}    
+		searchMovie(userInput);
+		break;
+	case 'do-what-it-says':
+		doWhatItSays();
+		break;
+	default: 
+		console.log("Please type any of the following commands: my-tweets spotify-this-song movie-this do-what-it-says.")
 }
-runCommands()
+
+// function to get tweets
+function myTweets() {
+	// console.log('making sure myTweets runs')
+//---debug: code32--could not authenticate you.  misspelled 'tiwitter' in keys.js---//
+	// console.log(JSON.stringify(keys.twitter))
+	var params = {screen_name: 'ucb_zeelee28'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    // console.log(tweets);
+	    for (var i = 0; i < tweets.length; i++) {
+	    	console.log('@'+tweets[i].user.screen_name+'\ntweeted: '+tweets[i].text)
+	    	console.log('at '+tweets[i].created_at)
+	    	console.log('===============================')
+	    }
+	  }else{console.log(error)}
+	})
+}
